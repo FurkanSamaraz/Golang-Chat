7a008dc51196acaf53d0e3cf3e96e76964627611
Furkan Samaraz <92402372+FurkanSamaraz@users.noreply.github.com>
2023-05-14 01:51:10 +0300

    successful

--- docs/docs.go
+++ docs/docs.go
@@ -14,7 +14,7 @@ const docTemplate = `{
         "version": "{{.Version}}"
     },
     "host": "{{.Host}}",
-    "basePath": "{{.BasePath}}",
+    "basePath": "{{.BasePath}}", 
     "paths": {
         "/chat-history": {
             "get": {
